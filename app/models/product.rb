31264769827fee3f1071cc565852f952ef57b586
T-yosi21 <r.life0726@gmail.com>
2020-10-08 14:58:18 +0900

    DB_create_2

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
- has_many :images
- belongs_to :user
- has_many :orders
- belongs_to :category
+  has_many :images
+  belongs_to :user
+  has_many :orders
+  belongs_to :category
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :delivery_burden
   belongs_to_active_hash :product_status
