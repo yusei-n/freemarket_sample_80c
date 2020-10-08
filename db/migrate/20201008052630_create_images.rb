0400a07a8efcbe742fadbce8ad61504188e23a4b
T-yosi21 <r.life0726@gmail.com>
2020-10-08 19:06:17 +0900

    DB_修正

--- db/migrate/20201008052630_create_images.rb
+++ db/migrate/20201008052630_create_images.rb
@@ -1,7 +1,7 @@
 class CreateImages < ActiveRecord::Migration[6.0]
   def change
     create_table :images do |t|
-      t.references :product, foreign_key: true
+      t.references :product, foreign_key: true, null: false
       t.text :image
       t.timestamps
     end
