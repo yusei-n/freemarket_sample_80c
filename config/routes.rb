163e49f33fc3b206d449f049878a14edbb4ea91d
T-yosi21 <r.life0726@gmail.com>
2020-10-16 17:16:48 +0900

    BEM Fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root 'products#index'
+  root 'orders#show'
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :edit, :create, :new, :update] do
