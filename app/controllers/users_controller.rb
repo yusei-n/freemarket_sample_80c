e4894bca1848ea064770033f45cad6887e609c92
kurumin27 <oneokrock_20@yahoo.co.jp>
2020-10-22 22:26:43 +0900

    login logout display switching

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,17 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
 
   def show
   end
 
   def new
+    @user = current_user.id
   end
 
   def create
   end
 
   def edit
-
   end
 
   def update
