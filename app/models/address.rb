a70b46e9557c61a47b8ef80189d6f50650cb829b
tasato1150 <tamtamuuu@gmail.com>
2020-10-24 00:10:07 +0900

    user-sign-in

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,7 @@
 class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :postal_prefectures
-  belongs_to :user
+  belongs_to :user, optional: true
+
+  validates :last_name, :first_name, :last_name_hurigana, :first_name_hurigana, :birthday, :postal_number, :postal_prefectures_id, :postal_municipalities, :postal_address, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_scope :user do
+    get  'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
   root 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :edit, :create, :new, :update] do
     resources :cards, only: [:new, :create, :delete, :show]
-    resources :addresss, only: [:create, :edit, :update, :new]
+    resources :addresses, only: [:create, :edit, :update, :new]
     member do
       get :logout
     end
