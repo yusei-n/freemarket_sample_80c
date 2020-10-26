1abc64e4bcae7b4c0ca1648692af7f256108af4e
tasato1150 <tamtamuuu@gmail.com>
2020-10-26 20:59:19 +0900

    error-message

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,5 +3,17 @@ class Address < ApplicationRecord
   belongs_to_active_hash :postal_prefectures
   belongs_to :user, optional: true
 
-  validates :last_name, :first_name, :last_name_hurigana, :first_name_hurigana, :birthday, :postal_number, :postal_prefectures_id, :postal_municipalities, :postal_address, presence: true
+  validates :last_name, presence: true,
+                        format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください" }
+  validates :first_name, presence: true,
+                         format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください" }
+  validates :last_name_hurigana, presence: true,
+                                 format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+  validates :first_name_hurigana, presence: true,
+                                  format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+  validates :birthday, presence: true
+  validates :postal_number, presence: true
+  validates :postal_prefectures_id, presence: true
+  validates :postal_municipalities, presence: true
+  validates :postal_address, presence: true
 end
