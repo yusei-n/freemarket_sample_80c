546079deecb7aabee68b801b4895d715daef75e0
T-yosi21 <r.life0726@gmail.com>
2020-12-07 21:52:11 +0900

    Update cards_controller.rb

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,7 +1,7 @@
 class CardsController < ApplicationController
   require "payjp"
   before_action :set_card
-  def index #CardのデータをPayjpに送って情報を取り出す
+  def index
     if @card.present?
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(@card.customer_id)
@@ -31,15 +31,15 @@ class CardsController < ApplicationController
   def new 
   end
 
-  def create #PayjpとCardのデータベースを作成
+  def create 
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
     if params['payjp-token'].blank?
       redirect_to action: "new",alert: "トークンが生成されていません"
     else
       customer = Payjp::Customer.create(
-        description: "ニックネーム：" + current_user.nickname, # PAY.JPの顧客情報に表示する概要。
+        description: "ニックネーム：" + current_user.nickname, 
         email: current_user.email,
-        card: params['payjp-token'], # 直前のnewアクションで発行され、送られてくるトークンをここで顧客に紐付けて保存。
+        card: params['payjp-token'], 
         metadata: {user_id: current_user.id} 
       )
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
@@ -51,13 +51,13 @@ class CardsController < ApplicationController
     end
   end
 
-  def delete #PayjpとCardのデータベースを削除
+  def delete 
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
     customer = Payjp::Customer.retrieve(@card.customer_id)
     customer.delete
-    if @card.destroy #削除に成功した時にポップアップを表示
+    if @card.destroy 
       redirect_to action: "index"
-    else #削除に失敗した時にアラートを表示
+    else 
       redirect_to action: "index", alert: "削除できませんでした"
     end
   end
